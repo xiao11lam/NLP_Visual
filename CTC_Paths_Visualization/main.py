cbb57d76d318f0e7031be04e093f8880c8725a50
xiao11lam <xiaoelevenlam@gmail.com>
2022-06-16 18:34:45 +0800

    add more

--- CTC_Paths_Visualization/main.py
+++ CTC_Paths_Visualization/main.py
@@ -11,6 +11,6 @@ for path_num_a in range(len(a)):
             for path_num_d in range(len(d)):
                 for path_num_e in range(len(e)):
                     for path_num_f in range(len(f)):
-                        print(a[path_num_a], b[path_num_b], c[path_num_c], d[path_num_d], e[path_num_e], f[path_num_f])
+                        print(a[path_num_a],b[path_num_b],c[path_num_c],d[path_num_d],e[path_num_e],f[path_num_f])
     # for a[i], j in enumerate(a):
         # print(a[i],j)
